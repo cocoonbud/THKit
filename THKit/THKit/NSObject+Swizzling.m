880c6cd0bb62eb723122dc3f598a6b5d561b997e
cocoon <yangle@thejoyrun.com>
2021-08-03 17:00:23 +0800

    add NSObject+swizzling add VC+backbuttonHandler remove VC+backclick

---
+++ THKit/THKit/NSObject+Swizzling.m
@@ -0,0 +1,29 @@
+//
+//  NSObject+Swizzling.m
+//  THKit
+//
+//  Created by Terence Yang on 2021/8/3.
+//
+
+#import "NSObject+Swizzling.h"
+#import <objc/message.h>
+
+@implementation NSObject (Swizzling)
+
++ (void)methodSwizzlingWithOriginalSelector:(SEL)originalSelector bySwizzlingedSelector:(SEL)swizzledSelector {
+    Class class = [self class];
+    
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    
+    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+    
+    BOOL addM = class_addMethod(class, swizzledSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+    
+    if (addM) {
+        class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+    } else{
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+}
+
+@end
